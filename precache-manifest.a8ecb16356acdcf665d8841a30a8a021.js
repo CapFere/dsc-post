b48e5b3c8fb67c2ad3a2359672e069387caf7a0b
Fireayehu Zekarias <fireayehuzekarias1989@gmail.com>
2019-12-27 15:20:11 +0300

    Updates

---
+++ precache-manifest.a8ecb16356acdcf665d8841a30a8a021.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c515d0e31a6bf8ff949dbb433386b2c2",
+    "url": "/dsc-post/index.html"
+  },
+  {
+    "revision": "e1c3410252e762c57a17",
+    "url": "/dsc-post/static/css/main.c06c350f.chunk.css"
+  },
+  {
+    "revision": "a6f1d2e371f964762c6c",
+    "url": "/dsc-post/static/js/2.61a4ff3f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dsc-post/static/js/2.61a4ff3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1c3410252e762c57a17",
+    "url": "/dsc-post/static/js/main.2528adf3.chunk.js"
+  },
+  {
+    "revision": "5a41bcaa361ded80756b",
+    "url": "/dsc-post/static/js/runtime-main.021ba72c.js"
+  }
+]);
